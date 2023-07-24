8b2f88b236069abe212ad141341555f3802b7473
Анжелика Демина <angelika@MacBook-Pro-Anzelika-3.local>
2023-07-25 01:24:03 +0300

    develop

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
-from .models import Recipe, Tag, ShoppingCart
-from django.contrib.auth import get_user_model
-from rest_framework.serializers import (ModelSerializer, SerializerMethodField,
-                                        ValidationError)
+from .models import Recipe, Tag, ShoppingCart, Ingredient, RecipeIngredient, FavoriteRecipe
 from drf_extra_fields.fields import Base64ImageField
 from users.models import CustomUser, Follow
 from djoser.serializers import UserCreateSerializer, UserSerializer
+from django.shortcuts import get_object_or_404
 
 
-User = get_user_model()
 class CustomUserCreateSerializer(UserCreateSerializer):
 
     class Meta:
@@ -16,29 +13,19 @@ class CustomUserCreateSerializer(UserCreateSerializer):
         fields = (
             'email', 'username', 'first_name',
             'last_name', 'password')
+        
     def validate_username(self, value):
-        print("paботает \n \n \n\n")
-        # username = value
-        # if not re.match(r'^[\w.@+-]+\z',username):
-        #     ValidationError(
-        #         "Недопустимые символы"
-        #     )
+        if not re.match(r'^[\w.@+-]+$', value):
+            raise serializers.ValidationError("Недопустимые символы.")
         return value
 
+
 class CustomUserSerializer(UserSerializer):
-    is_subscribed = SerializerMethodField(read_only=True)
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = CustomUser
-        fields = (
-            'email',
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'is_subscribed',
-            'password',
-        )
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_subscribed']
         extra_kwargs = {
             'password': {'write_only': True},
         }
@@ -48,72 +35,125 @@ class CustomUserSerializer(UserSerializer):
             return False
         return user.follow.filter(id=author.id).exists()
 
+
 class TagSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Tag
         fields = ['id', 'name', 'color', 'slug']
 
-class RecipeSerializer(serializers.ModelSerializer):
+
+class IngredientSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name', 'measurement_unit']
+
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+
+    id = serializers.ReadOnlyField(source="ingredient.id")
+    name = serializers.ReadOnlyField(source="ingredient.name")
+    amount = serializers.ReadOnlyField()
+    measurement_unit = serializers.ReadOnlyField(
+        source="ingredient.measurement_unit"
+    )
+    class Meta:
+        model = RecipeIngredient
+        fields = ("id", "name", "amount", "measurement_unit")
+    
+class RecipeCreateSerializer(serializers.ModelSerializer):
+
     image = Base64ImageField()
     tags = TagSerializer(read_only=True, many=True)
     author = CustomUserSerializer(read_only=True)
-    # is_favorited = serializers.SerializerMethodField()
-    # is_in_shopping_cart = serializers.SerializerMethodField()
+    ingredients = RecipeIngredientSerializer(many=True, read_only=True)
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
 
-    # def get_is_favorited(self, obj):
-    #     # Implement your logic to determine if the recipe is favorited
-    #     # For example, you can check if the recipe is in the user's favorites
-    #     # and return True or False accordingly.
-    #     user = self.context['request'].user
-    #     return obj.is_favorited_by_user(user)  #
     class Meta:
         model = Recipe
-        fields = ['id', 'name', 'image', 'cooking_time', 'tags', 'author']
+        fields = ['id', 'name', 'image', 'cooking_time', 'tags', 'author', 'is_in_shopping_cart', 'is_favorited', 'ingredients', 'text']
 
-    # def is_favorited(self, recipe):
-    #     return recipe.is_favorited.count()
+    def get_is_favorited(self, obj):
+        user = self.context['request'].user
+        if not user.is_authenticated:
+            return False
+        return FavoriteRecipe.objects.filter(user=user).exists()
 
-class RecipeDetailSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
+    def get_is_in_shopping_cart(self, obj):
+        user = self.context['request'].user
+        if not user.is_authenticated:
+            return False
+        return ShoppingCart.objects.filter(user=user).exists()
+    
+    def create(self, validated_data):
+        ingredients = self.initial_data.get('ingredients', [])
+        tags_data = self.context['request'].data.get('tags', [])
+        recipe = Recipe.objects.create(
+            author=self.context['request'].user,
+            name=validated_data['name'],
+            image=validated_data['image'],
+            cooking_time=validated_data['cooking_time'],
+            text=validated_data['text']
+        )
+        for tag_data in tags_data:
+            recipe.tags.add(get_object_or_404(Tag, pk=tag_data))
+
+        for ingredient in ingredients:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=ingredient['id'],
+                amount=ingredient['amount']
+            )
+        return recipe
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        ingredients_data = RecipeIngredient.objects.filter(recipe=instance)
+        data['ingredients'] = RecipeIngredientSerializer(ingredients_data, many=True).data
+        return data
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
 
     class Meta:
         model = Recipe
         fields = ['id', 'name', 'image', 'cooking_time']
 
+
 class ShoppingCartSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = ShoppingCart
         fields = ('user', 'recipe',)
     
 class FollowSerializer(UserSerializer):
-    recipes = SerializerMethodField(read_only=True)
-    recipes_count = SerializerMethodField()
+    recipes = serializers.SerializerMethodField(read_only=True)
+    recipes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Follow
         fields = '__all__',
-        # (
-        #     'email',
-        #     'id',
-        #     'username',
-        #     'first_name',
-        #     'last_name',
-        #     'is_subscribed',
-        #     'recipes',
-        #     'recipes_count',
-        # )
         read_only_fields = '__all__',
+    
+    def get_recipes_count(self, author):
+        return author.recipes.count()
 
-    # def get_is_subscribed(*args):
-    #     return True
+    def get_recipes_count(self, obj):
+        return Recipe.objects.filter(author=obj).count()
 
-    # def get_recipes_count(self, author):
-    #     return author.recipes.count()
-
-    # def get_recipes(self, obj):
-    #     request = self.context.get('request')
-    #     recipes = obj.recipes.all()
-    #     limit = request.query_params.get('recipes_limit')
-    #     if limit:
-    #         recipes = recipes[:int(limit)]
-    #     return RecipeSerializer(recipes, many=True).data
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        author_id = self.context.get('request').parser_context.get(
+            'kwargs').get('user_id')
+        current_user = request.user
+        author = get_object_or_404(CustomUser, pk=author_id)
+        Follow.objects.create(user=current_user, author=author)
+        return author
+    
+class FavoriteRecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavoriteRecipe
+        fields = '__all__'
--- backend/csv_import.py
+++ backend/csv_import.py
@@ -1,11 +1,10 @@
 import csv
 import os
 import django
+from api.models import Ingredient
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
 django.setup()
-
-from api.models import Ingredient
 
 def import_data_from_csv(csv_file):
     with open(csv_file, 'r', encoding='utf-8') as csvfile:
@@ -16,10 +15,12 @@ def import_data_from_csv(csv_file):
             existing_ingredient = Ingredient.objects.filter(name=name).first()
 
             if not existing_ingredient:
-                ingredient = Ingredient.objects.create(name=name, measurement_unit=measurement_unit)
+                ingredient = Ingredient.objects.create(name=name, 
+                                                       measurement_unit=measurement_unit)
                 print(f"Created ingredient: {ingredient}")
             else:
                 print(f"Ingredient '{name}' already exists in the database.")
+
 
 if __name__ == '__main__':
     csv_file_path = '../data/ingredients.csv'
