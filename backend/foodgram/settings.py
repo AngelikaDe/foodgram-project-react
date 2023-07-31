39106b74834b5b7212bc7075ce7039bec4410640
Анжелика Демина <angelika@MacBook-Pro-Anzelika-3.local>
2023-08-01 01:03:15 +0300

    linters

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('django-insecure--1gpdd*fekcse+cjdrxu+nt+s*b'
-              'j+gak+cro3eu!0hn_)9l=^l')
+SECRET_KEY = 'django-insecure--1gpdd*fekcse+cjdrxu+nt+s*bj+gak+cro3eu!0hn_)9l=^l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,23 +92,17 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'UserAttributeSimilarityValidator',
-            ),
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
 
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
